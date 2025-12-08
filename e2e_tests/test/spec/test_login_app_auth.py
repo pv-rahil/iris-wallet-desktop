73f42bc635235fb586070e6df8b8a9d0e2f5d771
pv-rahil <rahil@pointsville.com>
2025-12-08 18:04:19 +0530

    fix: Ensure login app authentication toggle is unchecked before assertion

--- e2e_tests/test/spec/test_login_app_auth.py
+++ e2e_tests/test/spec/test_login_app_auth.py
@@ -80,6 +80,13 @@ def test_login_app_with_authentication(wallets_and_operations: WalletTestSetup):
         wallets_and_operations.first_page_operations.enter_native_password()
 
         toggle_button = wallets_and_operations.first_page_objects.settings_page_objects.login_auth_toggle_button()
+        if getattr(
+            toggle_button, 'checked', None,
+        ) is True:
+            wallets_and_operations.first_page_objects.settings_page_objects.click_login_app_toggle_button()
+
+
+        toggle_button = wallets_and_operations.first_page_objects.settings_page_objects.login_auth_toggle_button()
         assert toggle_button is not None and getattr(
             toggle_button, 'checked', None,
         ) is False
